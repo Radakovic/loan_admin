d65ab21b1bf63239b1e68213189a26e088c95c5c
rade <radakovicradoslav@gmail.com>
2024-11-16 04:25:30 +0100

    created new component for red links

--- resources/views/client/partials/client-list-table.blade.php
+++ resources/views/client/partials/client-list-table.blade.php
@@ -28,12 +28,13 @@
 {{--                        {{ __('Edit') }}--}}
 {{--                    class="rounded-md px-3 py-2 dark:bg-red-500/20"--}}
 {{--                    </a>--}}
-                    <a
-                            href="{{ $client->id }}"
-                            class="inline-flex items-center px-4 py-2 bg-white dark:bg-red-500/20 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-red-500 dark:hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150"
-                    >
-                        {{ __('Delete') }}
-                    </a>
+{{--                    <a--}}
+{{--                            href="{{ $client->id }}"--}}
+{{--                            class="inline-flex items-center px-4 py-2 bg-white dark:bg-red-500/20 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-red-500 dark:hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150"--}}
+{{--                    >--}}
+{{--                        {{ __('Delete') }}--}}
+{{--                    </a>--}}
+                    <x-red-link-button route="{{ $client->id }}" text="{{ __('Delete') }}"></x-red-link-button>
                 </td>
             </tr>
         @endforeach
